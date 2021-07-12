900a123886cc5ece8c16945235d87e2d055b4b80
kainulj <jaakko.kainulainen@aalto.fi>
2021-07-12 12:42:00 +0300

    Gets weather information and displays temperature

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react/prop-types */
+import './App.css'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
-function App() {
+const Weather = ({ weather, forecast }) => {
+  if(!weather || !forecast){
+    return null
+  }
+  console.log(`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Temperature: {weather.main.temp}℃</h2>
+      <h2>Temperature: {forecast.lat}℃</h2>
+      <img
+        src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+        alt={weather.weather[0].description}
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+function App() {
+  const [city, setCity] = useState('Helsinki')
+  const [currentWeather, setCurrentWeather] = useState(null)
+  const [forecast, setForecast] = useState(null)
+
+  const getWeather = async () => {
+    try {
+      const weather = await axios.get(
+        `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_API_KEY}`
+      )
+      setCurrentWeather(weather.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getWeather()
+  }, [])
+
+  useEffect(() => {
+    if(currentWeather){
+      const coords = currentWeather.coord
+      axios
+        .get(
+          `http://api.openweathermap.org/data/2.5/onecall?lat=${coords.lat}&lon=${coords.lon}&exclude=current,minutely&units=metric&appid=${process.env.REACT_APP_API_KEY}`
+        )
+        .then(response => setForecast(response.data))
+    }
+  }, [currentWeather])
+
+  const handleSubmit =  (event) => {
+    event.preventDefault()
+    getWeather()
+  }
+
+  const changeCity = (event) => {
+    setCity(event.target.value)
+  }
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <input name="city" defaultValue={city} onChange={changeCity}></input>
+        <button type="submit">Search</button>
+      </form>
+      <Weather weather={currentWeather} forecast={forecast}/>
+    </div>
+  )
+}
+
+export default App
