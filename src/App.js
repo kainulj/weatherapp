57e68706d62cd7e06ae5c0e935650e21e5cc70dc
kainulj <jaakko.kainulainen@aalto.fi>
2021-07-19 19:58:39 +0300

    Hourly forecast

--- src/App.js
+++ src/App.js
@@ -2,21 +2,69 @@
 import './App.css'
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import { AppBar, Tabs, Tab, Table, TableRow, TableCell, TableBody, TableContainer, TableHead } from '@material-ui/core'
+
+const HourlyForecast = ({ forecast, tab, index }) => {
+  const hour = new Date().getHours() + 1
+  return (
+    <TableContainer hidden={tab !== index}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell></TableCell>
+            <TableCell></TableCell>
+            <TableCell>Temperature</TableCell>
+            <TableCell>Feels like</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {forecast.slice(1,24).map((row, i) => (
+            <TableRow key={i}>
+              <TableCell>{`${(hour+i) % 24}:00`}</TableCell>
+              <TableCell>
+                <img
+                  src={`http://openweathermap.org/img/wn/${row.weather[0].icon}@2x.png`}
+                  alt={row.weather[0].description}
+                />
+              </TableCell>
+              <TableCell>{row.temp} ℃</TableCell>
+              <TableCell>{row.feels_like} ℃</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+}
 
 const Weather = ({ weather, forecast }) => {
+  const [tab, setTab] = useState(0)
+
   if(!weather || !forecast){
     return null
   }
-  console.log(`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`)
+
+  const handleTabChange = (event, newValue) => {
+    setTab(newValue)
+  }
 
   return (
     <div>
-      <h2>Temperature: {weather.main.temp}℃</h2>
-      <h2>Temperature: {forecast.lat}℃</h2>
+      <h1>{weather.name}</h1>
       <img
         src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
         alt={weather.weather[0].description}
       />
+      <h2>{weather.weather[0].description}</h2>
+      <h2>Temperature: {weather.main.temp}℃</h2>
+      <h2>Feels like: {weather.main.feels_like}℃</h2>
+      <AppBar position="static">
+        <Tabs value={tab} onChange={handleTabChange}>
+          <Tab label='hourly'/>
+          <Tab label='Daily'/>
+        </Tabs>
+        <HourlyForecast forecast={forecast.hourly} tab={tab} index={0}/>
+      </AppBar>
     </div>
   )
 }
@@ -57,14 +105,14 @@ function App() {
     getWeather()
   }
 
-  const changeCity = (event) => {
+  const handleCityChange = (event) => {
     setCity(event.target.value)
   }
 
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
-        <input name="city" defaultValue={city} onChange={changeCity}></input>
+        <input name="city" onChange={handleCityChange}></input>
         <button type="submit">Search</button>
       </form>
       <Weather weather={currentWeather} forecast={forecast}/>
